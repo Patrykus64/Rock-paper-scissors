a145e4d4913d0cbd5a8809f25a7819e612c85349
Patryk Golab <patrykus64@gmail.com>
2022-07-29 13:25:47 +0200

    Player and AI pick

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const playerWinsLSKey = "playerWins";
 const AIWinsLSKey = "AIWins"; // Nazwa klucza który będziemy wykorzystywać zapisując czy odczytując playerwins
 let state = {
     playerWins:localStorage.getItem(playerWinsLSKey)|| 0, //Na początku daliśmy tu zero ale teraz zmieniamy na localStorage aby odczytywać czy mamy w pamięci jakieś punkty już zrobione
-    AIWins: localStorage.getItem(AIWinsLSKey) || 0 //Kreski po localStorage mówią, że jezeli mamy jakąs wartośc zapisaną to ja tam wrzćmy jak nie to przypisz 0
+    AIWins: localStorage.getItem(AIWinsLSKey) || 0, //Kreski po localStorage mówią, że jezeli mamy jakąs wartośc zapisaną to ja tam wrzćmy jak nie to przypisz 0
+    playerPick: null,
+    AIPick: null
 };
 
 const renderScore = () => {
@@ -16,13 +18,35 @@ const renderScore = () => {
 };
 const bindPickEvents = () => {
     document.querySelectorAll(".options button").forEach((button)=> { //ForEach to dla każdego elementu jest wywołana funkcja
-        button.addEventListener("click", ()=>{
-                debugger;
-        });
+        button.addEventListener("click", (a)=>{
+
+            pickByPlayer(a.currentTarget.dataset.pick);
+            pickByAI();
+            console.log(state);
+                //console.log(a.target) // to pokazuje nam jaki przycisk klikneliśmy 
+        });     
 
     });
 
 };
+const pickByPlayer = (pickedOption) =>{
+    state = { // Nasz state to ten stary state na początku co deklarowaliśmy
+                // Ale teraz mały update gdzie playerPick nie ma już zera tylko pickedOption
+        ...state,
+        playerPick: pickedOption,
+    };
+};
+const pickByAI = () => {
+    const options =["rock","paper","scissors"];
+    const AIPick = options[Math.floor(Math.random() * options.length)];//math.random wybiera liczbe od 0 do 1
+    //pomnożone przez optionslength da liczbe od 0 do 3
+    //Mathfloor zaokrągla
+    stata = {
+        ...state,
+        AIPick //destructering czy coś takiego:D
+    };
+};
+
 
 const init = () => {
     renderScore();
